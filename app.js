9dbffaeec2eb885615eeb2d77b98e0d94a3b0c00
Niels <git@nierot.com>
2020-07-23 16:39:19 +0200

    everything looks nice

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const port = 8080;
-
+let users = new Map();
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/static/index.html')
@@ -13,17 +13,39 @@ app.get('/static/*', (req, res) => {
 })
 
 io.on('connection', socket => {
-    console.log('A user connected!');
+
+    this.username = undefined;
+    
+    socket.on('username', name => {
+        console.log(`User connected with name: ${name}`);
+        this.username = name;
+        users.set(socket, name);
+        io.emit('username', name);
+    })
     
     socket.on('chat message', msg => {
         console.log(`Message: ${msg}`);
-        io.emit('chat message', msg);
+        socket.broadcast.emit('chat message', {
+            text: msg,
+            date: new Date().toUTCString(),
+            user: this.username
+        });
     })
 
     socket.on('disconnect', () => {
-        console.log('A user has disconnected')
+        console.log('A user has disconnected');
+        disconnect(socket);
+        users.delete(socket);
     })
 })
+
+function disconnect(socket) {
+    console.log(users)
+    for (let user in users) {
+        console.log(user)
+        user.emit('user disconnect', users.get(socket))
+    }
+}
 
 http.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`)
---
+++ static/index.js
@@ -0,0 +1,46 @@
+function incomingMessage(msg) {
+    appendMessage(
+        `<div class="media w-50 mb-3">
+            <div class="media-body ml-3">
+            <div class="bg-light rounded py-2 px-3 mb-2">
+                <p class="text-small mb-0 text-muted">${msg.text}</p>
+            </div>
+            <p class="small text-muted">${msg.user} | ${msg.date}</p>
+            </div>
+        </div> `
+    );
+}
+
+function appendOwnMessage(msg) {
+    appendMessage(
+    `<div class="media w-50 ml-auto mb-3">
+        <div class="media-body">
+            <div class="bg-primary rounded py-2 px-3 mb-2">
+                <p class="text-small mb-0 text-white">${msg}</p>
+            </div>
+            <p class="small text-muted">${new Date().toUTCString()}</p>
+        </div>
+    </div>`
+    );
+}
+
+function appendMessage(msg) {
+    $('#chatBox').append(msg);
+    $("#chatBox").scrollTop($("#chatBox")[0].scrollHeight);
+}
+
+function newUser(user) {
+    $('#users').append(
+        `<a class="list-group-item list-group-item-action text-grey rounded-0" id="user#${user}>
+            <div class="media-body ml-4">
+                <div class="d-flex align-items-center justify-content-between mb-0">
+                    <h6 class="mb-0">${user}</h6>
+                </div>
+            </div>
+        </a>`
+    );
+}
+
+function removeUser(user) {
+    $(`#user#${user}`).remove();
+}
